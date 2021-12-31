a991dbaacd417803aaf76bf145812dc13b4ff82f
elmerzouki <elmerzoukireda@gmail.com>
2021-12-31 07:43:40 -0800

    add file code.py

---
+++ code.py
@@ -0,0 +1,10 @@
+def find(num):
+    # code logic here
+    numtype = "odd"
+    if num%2 == 0:
+        numtype="even"
+    return numtype
+
+num = int(input('Enter the number: '))  
+numtype = find(num)                    
+print('Given number is',numtype)
